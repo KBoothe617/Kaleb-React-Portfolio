e59854f1ba5b96118028b3b4a395847fcbfb3d40
Kaleb Boothe <kboothe617@gmail.com>
2024-11-18 11:17:20 -0700

    footer.jsx github link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,8 @@ export default function Footer() {
         <footer className="custom-footer text-light py-3">
             <div className="container text-center">
                 {/* set icons and links */}
-                <a href="https://www.linkedin.com/in/kaleb-boothe-194881293/" className="linkedin-link me-4" aria-label="LinkedIn" style={{ fontSize: '2rem' }} target='_blank' rel="noopener noreferrer"> <FontAwesomeIcon icon={faLinkedin} /> </a>
+                <a href="https://www.linkedin.com/in/kaleb-boothe-194881293/" className="linkedin-link me-4" aria-label="LinkedIn" style={{ fontSize: '2rem' }} target='_blank' rel="noopener noreferrer"> <fontAwesomeIcon icon={faLinkedin} /> </a>
+                <a href="https://github.com/KBoothe617" className="github-link me-4" aria-label="GitHub" style={{ fontSize: '2rem' }} target='_blank' rel="noopener noreferrer"> <fontAwesomeIcon icon={faGithub} /> </a>
             </div>
         </footer>
     )
