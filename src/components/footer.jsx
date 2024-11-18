df32d8db531da5ae24d302c05162e8d547f222c2
Kaleb Boothe <kboothe617@gmail.com>
2024-11-18 10:59:34 -0700

    footer.jsx import statements and start of function

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import { fontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import './footer.css';
+
+export default function Footer() {
+    return (
+
+    )
+}
