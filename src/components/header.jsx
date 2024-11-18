de453973d394b5fb8787080a58718936cfa38b08
Kaleb Boothe <kboothe617@gmail.com>
2024-11-17 18:45:04 -0700

    header.jsx imported name and nav functions

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import Name from './name';
+import Nav from './nav';
+
+export default function Header() {
+    return (
+        <header>
+            <Name />
+            <Nav />
+        </header>
+    );
+}
