7d822da9c8d5d8cf5153979a9852e19d0e875a54
Kaleb Boothe <kboothe617@gmail.com>
2024-11-18 11:39:05 -0700

    aboutme.jsx aboutme function

---
+++ src/pages/aboutMe.jsx
@@ -0,0 +1,14 @@
+import './aboutMe.css';
+
+export default function AboutMe() {
+    return (
+        <section className="about-me">
+            <div classname="about-me-content">
+                <div className="about-me-bio">
+                    <h2>About Me</h2>
+                    <p></p>
+                </div>
+            </div>
+        </section>
+    );
+}
