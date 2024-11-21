b131ff6502a8c7dfce98cf20d398a3b98b2fb1ce
Kaleb Boothe <kboothe617@gmail.com>
2024-11-21 09:03:09 -0700

    aboutme.jsx added about me paragraph

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -6,7 +6,12 @@ export default function AboutMe() {
             <div classname="about-me-content">
                 <div className="about-me-bio">
                     <h2>About Me</h2>
-                    <p></p>
+                    <p>My name is Kaleb Lee Boothe Udy, I am a versatile programmer with expertise in Python, JavaScript, CSS, HTML,</p>
+                    <p>Node.js, and React. My journey into tech started with programming for FRC Robotics in high school, where I</p>
+                    <p>developed a passion for solving complex challenges through code. Today, I specialize in both frontend</p>
+                    <p>and backend development, crafting seamless and scalable applications that deliver exceptional user</p>
+                    <p>experiences. Whether it’s designing intuitive interfaces or building robust server-side solutions,</p>
+                    <p>side solutions, I thrive on turning ideas into reality. Let’s create something innovative together!</p>
                 </div>
             </div>
         </section>
